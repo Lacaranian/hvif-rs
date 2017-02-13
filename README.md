a268b76b9b71bb624d6eb9022c8b9e8048cd1511
Ben Elliott <benjam.elliott@gmail.com>
2017-02-12 21:51:56 -0700

    Pathing command parser, added another badge

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # hvif-rs
 
+[![LICENSE](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
 [![Travis Build Status](https://travis-ci.org/Lacaranian/hvif-rs.svg?branch=master)](https://travis-ci.org/Lacaranian/hvif-rs)
+[![Github All Releases](https://img.shields.io/github/downloads/Lacaranian/hvif-rs/total.svg)]()
 
 A Rust library for reading or writing icons in the Haiku Vector Icon Format
 
--- src/parser/path.rs
+++ src/parser/path.rs
@@ -22,8 +22,8 @@ fn hvif_path_parser_from_flags(input: &[u8], flags: u8, point_count: u8) -> IRes
     false => {
       let no_curves = HVIF_PATH_FLAG_NO_CURVES.is_set_on(flags);
       match no_curves {
-        true  => count!(input, hvif_path_point_with_no_curves, point_count as usize),
-        false => count!(input, hvif_path_point_with_curves, point_count as usize),
+        true  => count!(input, hvif_path_point_line, point_count as usize),
+        false => count!(input, hvif_path_point_curve, point_count as usize),
       }
     },
   }
@@ -48,18 +48,59 @@ named!(hvif_path_command_header_chunk<&[u8], Vec<u8>>,
 
 fn hvif_path_with_commands(input: &[u8], command_bytes: Vec<u8>) -> IResult<&[u8], Vec<HVIFPointCommand>>
 {
-  let size = command_bytes.len();
-  return IResult::Done(input, (vec![HVIFPointCommand::HLine { x: size as f32 }]))
+  let parsers: Vec<fn(&[u8]) -> IResult<&[u8], HVIFPointCommand>> = command_bytes.iter().flat_map(|&command_byte| {
+      let parser: Option<fn(&[u8]) -> IResult<&[u8], HVIFPointCommand>> = match command_byte {
+        0 => Some(hvif_path_point_horizontal_line),
+        1 => Some(hvif_path_point_vertical_line),
+        2 => Some(hvif_path_point_line),
+        3 => Some(hvif_path_point_curve),
+        _ => None,
+      };
+
+      parser
+  }).collect();
+
+  let result = if parsers.len() == command_bytes.len() {
+    // We don't have any incorrect commands! Parse away
+    // Imperative style required because nom's macros don't play well inside the closure of a fold
+    let mut piter = parsers.into_iter();
+    let mut points = Vec::new();
+    let mut rem_input = input;
+
+    while let Some(parser) = piter.next() {
+      let (next_input, point) = try_parse!(rem_input, parser);
+      rem_input = next_input;
+      points.push(point);
+    }
+
+    IResult::Done(rem_input, points)
+  } else {
+    // Incorrect command! Give an error
+    IResult::Error(ErrorKind::Custom(0))
+  };
+
+  result
 }
 
-named!(hvif_path_point_with_no_curves<&[u8], HVIFPointCommand>,
+named!(hvif_path_point_horizontal_line<&[u8], HVIFPointCommand>,
+  do_parse!(
+    x: hvif_path_coord >>
+    (HVIFPointCommand::HLine { x: x })
+  )
+);
+named!(hvif_path_point_vertical_line<&[u8], HVIFPointCommand>,
+  do_parse!(
+    y: hvif_path_coord >>
+    (HVIFPointCommand::VLine { y: y })
+  )
+);
+named!(hvif_path_point_line<&[u8], HVIFPointCommand>,
   do_parse!(
     point: hvif_path_point >>
     (HVIFPointCommand::Line { point: point })
   )
 );
-
-named!(hvif_path_point_with_curves<&[u8], HVIFPointCommand>,
+named!(hvif_path_point_curve<&[u8], HVIFPointCommand>,
   do_parse!(
     point: hvif_path_point >>
     point_in: hvif_path_point >>
