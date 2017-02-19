ba644d030b232b4ba7ba9a90d16ff4f35a8c781d
Ben Elliott <benjam.elliott@gmail.com>
2017-02-18 17:21:22 -0700

    Added little f24 to f32 conversion
    Might not be quite right... should probably add tests

--- src/types/shape.rs
+++ src/types/shape.rs
@@ -25,7 +25,7 @@ pub enum HVIFShapeModifier {
   /// Represents the allowable scales at which this shape will appear correctly
   HVIFLODScale {
     #[doc = "Minimum LOD for this shape"] min: f32,
-    #[doc = "Maximum LOD for this shape"] max: f32
+    #[doc = "Maximum LOD for this shape"] max: f32,
   },
   /// An optional linear translation of the shape (offset)
   HVIFTranslation(HVIFPoint),
@@ -40,6 +40,33 @@ pub struct HVIFf24 {
   #[doc = "First (most significant) bits"] pub fst: u8,
   #[doc = "Middle bits"] pub snd: u8,
   #[doc = "Last (least significant) bits"] pub thr: u8
+}
+impl From<HVIFf24> for f32 {
+  // Not quite exact in its current form - be warned!
+  fn from(hvif_f24: HVIFf24) -> Self {
+    let f24_as_u32: u32 = ((hvif_f24.fst as u32) << 16) | ((hvif_f24.snd as u32) << 8) | (hvif_f24.thr as u32);
+
+    // Get the raw bits for each independent component
+    let sign_bit      = (f24_as_u32 & 0b100000000000000000000000) >> 23;
+    let exponent_bits = (f24_as_u32 & 0b011111100000000000000000) >> 17;
+    let mantissa_bits =  f24_as_u32 & 0b000000011111111111111111;
+
+    // The sign is just -1 to the power of this bit
+    let sign : i32 = match sign_bit {
+      0 =>  1,
+      _ => -1,
+    };
+
+    // The exponent is actually only 6 bits, so unsigned values range from 0 to 63
+    // Make it a signed exponent, and offset it to range from -31 to 32
+    let exponent_pre = (exponent_bits as i32) - 31;
+
+    // The mantissa is 1.(bits), so we can divite the exponentiation into whole and fractional parts
+    let mantissa_base = f32::powf(2.0, exponent_pre as f32);
+    let mantissa_frac = (mantissa_bits as f32) * f32::powf(2.0, (exponent_pre -17) as f32);
+
+    (sign as f32) * (mantissa_base + mantissa_frac)
+  }
 }
 
 
